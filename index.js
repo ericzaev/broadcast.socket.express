1f1ee23e7afab2a7212547882a3d0d02f423afe2
eric <eric@bk.kz>
2022-07-03 22:23:53 +0600

    room namespace

--- index.js
+++ index.js
@@ -6,16 +6,26 @@ const rest = require('./rest');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
-app.post('/events/:user/:event', (request, response) => {
+app.use(function (request, response, next) {
   const token = process.env.ACCESS_TOKEN;
 
   if (token && token === request.query['access_token']) {
-    io.of('/user').to(parseInt(request.params.user)).emit(request.params.event, request.body);
+    next();
   } else {
     response.status(403);
+    response.end();
   }
+});
 
+app.post('/user/:user/:event', (request, response) => {
+  io.of('/user').to(parseInt(request.params.user)).emit(request.params.event, request.body);
+  
+  response.end();
+});
+
+app.post('/room/:room/:event', (request, response) => {
+  io.of('/room').to(request.params.room).emit(request.params.event, request.body);
+  
   response.end();
 });
 
@@ -29,18 +39,33 @@ io.of('/user').use(async (socket, next) => {
       if (socket.handshake.headers['cookie']) {
         socket.user = await rest.fetchUserByCookie(socket.handshake.headers['cookie']);
       }
-    } catch (error) {
-      next(new Error('Unauthenticated'));
-    }
+    } catch (error) {}
+  }
 
-    if (socket.user) {
-      next();
-    } else {
-      next(new Error('Unauthenticated'));
-    }
+  if (socket.user) {
+    next();
+  } else {
+    next(new Error('Unauthenticated'));
+  }
+});
+
+io.of('/room').use(async (socket, next) => {
+  if (!socket.room) {
+    try {
+      if (socket.handshake.auth['code']) {
+        socket.room = await rest.fetchRoomByCode(socket.handshake.auth['code']);
+      }
+    } catch (error) {}
+  }
+
+  if (socket.room) {
+    next();
+  } else {
+    next(new Error('Room not found'));
   }
 });
 
 io.of('/user').on('connection', socket => socket.join(socket.user.id));
+io.of('/room').on('connection', socket => socket.join(socket.room.code));
 
 http.listen(3000);
--- rest.js
+++ rest.js
@@ -1,9 +1,13 @@
 const axios = require('axios');
-const endpoint = process.env.USER_ENDPOINT || 'http://localhost/restful/v1/user/profile';
+const user_endpoint = process.env.USER_ENDPOINT || 'http://localhost/restful/v1/user/profile/';
+const room_endpoint = process.env.ROOM_ENDPOINT || 'http://localhost/restful/v1/chat/bearer/';
 
 module.exports = {
+    fetchRoomByCode: async function(code) {
+        return (await axios.get(room_endpoint, {params: {code}})).data;
+    },
     fetchUserByToken: async function(token) {
-        return (await axios.get(endpoint, {
+        return (await axios.get(user_endpoint, {
             params: {token},
             headers: {
                 Authorization: 'Bearer ' + token
@@ -11,6 +15,6 @@ module.exports = {
         })).data;
     },
     fetchUserByCookie: async function(Cookie) {
-        return (await axios.get(endpoint, {headers: {Cookie}})).data;
+        return (await axios.get(user_endpoint, {headers: {Cookie}})).data;
     }
 };
